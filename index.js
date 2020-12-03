b5a0118dd8a409a7bee4e5591a7f13b9d92c3140
ziggabyte <ziggi.willpower@gmail.com>
2020-12-03 17:19:36 +0100

    rensat dubletter och sorterat koden

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
 "use strict"
-//feature_list_note
 
+// ALLA VARIABLER ***************************************************************************************************************
 
-/* create content for the site using JS*/
-
+// KONSTANTER
 const container = document.getElementById("container"); // sparar container i en variabel
 const secondContainer = document.getElementById("secondContainer") //sparar secondContainer i en variabel
 const innerModal = document.createElement("div");// inre divbox i modalen
 
+// KNAPPAR FÖR DE OLIKA ANTECKNINGSTYPERNA
 let newEmptyListButton = document.createElement("button"); //button is created to initialize new list
 newEmptyListButton.setAttribute("id", "newEmptyListButton");
 newEmptyListButton.textContent = "New empty list";
-document.getElementById("container").appendChild(newEmptyListButton);
-container.appendChild(newEmptyListButton);
 
-function showNewList() {						//function displays list
-    listTitle.style.display = "block";
-    listHeading.style.display = "block";
-}
+let emptyNoteButton = document.createElement("button"); // skapar ett nytt button-element
+emptyNoteButton.setAttribute("id", "emptyNoteButton"); // ger det nya button-elementet id="emptyNoteButton"
+emptyNoteButton.textContent = "New empty note"; // sätter knappens text till New empty note
 
-function hideNewEmptyListButton() {
-    newEmptyListButton.style.display = "none"; //function hides newEmptyListButton
-}
+let textSuggestionButton = document.createElement("button");
+textSuggestionButton.setAttribute("id", "textSuggestionButton"); // ger det nya button-elementet id
+textSuggestionButton.textContent = "New note with text suggestion"; // sätter knappens text
 
+// SAVE-BUTTONS
+let saveBtn = document.createElement("button");
+saveBtn.setAttribute("id", "listSaveBtn");
+saveBtn.style.display = "none";
+saveBtn.innerText = "Save";
 
+let saveButton = document.createElement("button"); // skapar ett nytt button-element
+saveButton.setAttribute("id", "saveButton"); // ger detta nya button-element id="saveButton"
+saveButton.style.display = "none"; // ger den visibility: none, så att den är osynlig
+saveButton.textContent = "Save"; // ger den texten Save
 
-/* create content to for the site using JS*/
-
+// FÄLT TILL LISTOR 
 let listTitle = document.createElement("h3"); //sub-heading//
 listTitle.setAttribute("id", "listTitle");
 listTitle.innerText = "Add your Title:";
-document.getElementById("container").appendChild(listTitle);
-container.appendChild(listTitle);
 listTitle.style.display = "none"; // display = 'block' from eventlistener (DAN)
 
 let inputTitleBox = document.createElement("input"); //input field for user title//
 inputTitleBox.setAttribute("id", "inputTitleBox");
 inputTitleBox.setAttribute("type", "text");
 inputTitleBox.setAttribute("placeholder", "Title");
-document.getElementById("listTitle").appendChild(inputTitleBox);
 
 let userTitle = document.createElement("h4");
 userTitle.setAttribute("class", "userTitle");
@@ -47,11 +49,6 @@ userTitle.style.display="none";
 let listHeading = document.createElement("h3"); //subheading//
 listHeading.setAttribute("id", "listHeading");
 listHeading.innerText = "Add your entry:";
-document.getElementById("container").appendChild(listHeading);
-let listHeading = document.createElement("h3"); //subheading//
-listHeading.setAttribute("id", "listHeading");
-listHeading.innerText = "Add your entry:";
-container.appendChild(listHeading);
 listHeading.style.display = "none"; //display = 'block' from eventlistener (DAN)
 
 let inputItemBox = document.createElement("input"); //input field for user list items
@@ -59,17 +56,182 @@ inputItemBox.setAttribute("id", "inputListBox");
 inputItemBox.setAttribute("type", "text");
 inputItemBox.setAttribute("placeholder", "List Entry");
 inputItemBox.attributes.required = "required"; //!oklart om denna gör något!//
-document.getElementById("listHeading").appendChild(inputItemBox);
 
 let listNote = document.createElement("ul"); //unordered list
 listNote.setAttribute("class", "myList"); //om id, numereriskt som ökar för varje lista?
-document.getElementById("secondContainer").appendChild(listNote);
 
-let saveBtn = document.createElement("button");
-saveBtn.setAttribute("id", "listSaveBtn");
-saveBtn.style.display = "none";
-saveBtn.innerText = "Save";
-document.getElementById("secondContainer").appendChild(saveBtn);
+let inputTitle = document.createElement("h4"); //skapar en titel som user valt
+inputTitle.setAttribute("class", "userTitle");
+inputTitle.setAttribute("id", "myTitle");
+inputTitle.style.display = "none";
+
+// TEXTAREAS
+let newTextArea = document.createElement("textarea");
+newTextArea.style.display = "none";
+
+let newPromptTextArea = document.createElement("textarea");
+
+// MODALENS TRANSPERENTA BAKGRUND
+let modalBg = document.createElement("div");
+
+// ALLA FUNKTIONER ************************************************************************************************************
+
+//function to clear input field//
+function clearInput(input) {
+    input.value = "";
+}
+
+//function to add remove button to every list item//
+function addRemoveBtn (){
+    let items = document.querySelectorAll(".myListItem");
+    let removeBtn = document.createElement("button");
+    removeBtn.setAttribute("class", "removeListItem");
+    removeBtn.innerText = "X";
+
+    for (let i = 0; i < items.length; i++){
+        document.getElementsByClassName("myListItem")[i].appendChild(removeBtn);
+    }
+}
+
+/// ((( ALLA FEM FUNKTIONER HÄR UNDER SKULLE KUNNA ERSÄTTAS MED TVÅ, EN SOM VISAR OBJEKT, O EN SOM DÖLJER)))
+
+/**
+ * Döljer knapp
+ */
+function hideButton(button) {
+    button.style.display = "none";
+}
+
+/**
+ * Visar knapp
+ */
+
+function showButton(button) {
+    button.style.display = "block";
+}
+
+function showNewList() { //function displays list
+    listTitle.style.display = "block";
+    listHeading.style.display = "block";
+}
+
+function hideNewEmptyListButton() {
+    newEmptyListButton.style.display = "none"; //function hides newEmptyListButton
+}
+
+/**
+ * Visar text area vid tryck på "new note"
+*/
+function showTextArea() {
+    newTextArea.style.display = "block";
+}
+
+/**
+* Rensar text area vid tryck på "save"
+*/
+function clearTextArea() {
+    newTextArea.value = '';
+}
+
+/**
+ * Skapar en text area, lägger till ett textförslag och appendar den till container-div:en
+ */
+function openSuggestionTextArea() {
+    newPromptTextArea.value = randomTextSuggestion();
+    innerModal.appendChild(newPromptTextArea);
+}
+
+function modal() {
+    
+    let body = document.querySelector("body");
+    modalBg.setAttribute("id", "modalBgBox");
+    
+    modalBg.style.backgroundColor = "rgba(0,0,0,0.4)"; /* Black w/ opacity */
+    modalBg.style.width = "100%";
+    modalBg.style.height = "100vh";
+    modalBg.style.position = "absolute";
+    modalBg.style.top = "0px";
+    modalBg.style.padding = "100px";
+
+    innerModal.style.backgroundColor = "white";
+    innerModal.style.width = "50%";
+    
+    
+    body.appendChild(modalBg);
+    modalBg.appendChild(innerModal);
+}
+
+function closeModal (e){
+    if (e.target === modalBg){
+        modalBg.style.display = "none";
+    }
+}
+
+/**
+ * Slumpar fram ett av 10 textförslag
+ * Returnerar en string med ett textförslag
+ */
+function randomTextSuggestion() {
+    let number = Math.round((Math.random() * 10) + 1);
+    let text;
+    switch (number) {
+        case 1:
+            text = "What did you hate most about 2020?";
+            break;
+        case 2:
+            text = "Where do you want to travel when Corona restrictions end?";
+            break;
+        case 3:
+            text = "What people have you missed during this year, and how do you plan to connect more with them in the future?";
+            break;
+        case 4:
+            text = "What have you learned about yourself during 2020?";
+            break;
+        case 5:
+            text = "Shows, music, artists you have discovered during 2020?";
+            break;
+        case 6:
+            text = "How has your life changed during 2020?";
+            break;
+        case 7:
+            text = "How have you taken care of yourself and others this year?";
+            break;
+        case 8:
+            text = "What has made you angry during 2020?";
+            break;
+        case 9:
+            text = "What do you dream about for 2021?";
+            break;
+        case 10:
+            text = "Who have you turned to this year? Why?";
+            break;
+        default:
+            text = "default";
+    }
+
+    return text;
+}
+
+// ALLA APPEND CHILD **************************************************************************************
+
+// KNAPPARNA FÖR OLIKA ANTECKNINGAR
+container.appendChild(newEmptyListButton);
+container.appendChild(emptyNoteButton); // Lägger till new empty note-knappen i container-div:en
+container.appendChild(textSuggestionButton); // Lägger till textSuggestionButton i container-div:en
+
+// LISTOR 
+container.appendChild(listTitle);
+container.appendChild(listHeading);
+secondContainer.appendChild(saveBtn);
+secondContainer.appendChild(listNote);
+
+document.getElementById("listTitle").appendChild(inputTitleBox);
+document.getElementById("listHeading").appendChild(inputItemBox);
+
+innerModal.appendChild(saveButton); // Lägger till save-knappen i container-div:en
+innerModal.appendChild(newTextArea);
+
+// ALLA EVENT LISTENERS ***********************************************************************************
 
 //function to add title chosen by user. Triggered when enter is released. 
 inputTitleBox.addEventListener("keyup", function (e) {
@@ -121,243 +283,17 @@ inputItemBox.addEventListener("keyup", function (e) {
     }
 });
 
-//function to clear input field//
-function clearInput(input) {
-    input.value = "";
-}
-
-//function to add remove button to every list item//
-function addRemoveBtn (){
-    let items = document.querySelectorAll(".myListItem");
-    let removeBtn = document.createElement("button");
-    removeBtn.setAttribute("class", "removeListItem");
-    removeBtn.innerText = "X";
-
-    for (let i = 0; i < items.length; i++){
-        document.getElementsByClassName("myListItem")[i].appendChild(removeBtn);
-    }
-}
-
-let inputTitle = document.createElement("h4"); //skapar en titel som user valt
-inputTitle.setAttribute("class", "userTitle");
-inputTitle.setAttribute("id", "myTitle");
-inputTitle.style.display = "none";
-
-
-/*
-//Add todays date to list note//
-let today = new Date();
-let date = today.getFullYear() + "-" + (today.getMonth()+1) + "-" + today.getDate();
-
-//constructor for note-object in list form//
-function ListNote(inputtitle, inputlistItem, date){
-    listTitle = inputtitle;
-    listItem = inputlistItem;
-    listDate = date;
-};*/
-
-//feature_list_saveButton
-
-//Ziggi ************************************************************************//
-
-let emptyNoteButton = document.createElement("button"); // skapar ett nytt button-element
-emptyNoteButton.setAttribute("id", "emptyNoteButton"); // ger det nya button-elementet id="emptyNoteButton"
-emptyNoteButton.textContent = "New empty note"; // sätter knappens text till New empty note
-
-let textSuggestionButton = document.createElement("button");
-textSuggestionButton.setAttribute("id", "textSuggestionButton"); // ger det nya button-elementet id
-textSuggestionButton.textContent = "New note with text suggestion"; // sätter knappens text
-
-let saveButton = document.createElement("button"); // skapar ett nytt button-element
-saveButton.setAttribute("id", "saveButton"); // ger detta nya button-element id="saveButton"
-saveButton.style.display = "none"; // ger den visibility: none, så att den är osynlig
-saveButton.textContent = "Save"; // ger den texten Save
-
-const container = document.getElementById("container"); // sparar container i en variabel
-container.appendChild(emptyNoteButton); // Lägger till new empty note-knappen i container-div:en
-container.appendChild(saveButton); // Lägger till save-knappen i container-div:en
-container.appendChild(textSuggestionButton); // Lägger till textSuggestionButton i container-div:en
-
-let newTextArea = document.createElement("textarea");
-container.appendChild(newTextArea);
-newTextArea.style.display = "none";
-
-/**
- * Visar text area vid tryck på "new note"
-*/
-function showTextArea() {
-    newTextArea.style.display = "block";
-}
-
-/**
-* Rensar text area vid tryck på "save"
-*/
-function clearTextArea() {
-    newTextArea.value = '';
-}
-
-container.appendChild(emptyNoteButton); // Lägger till new empty note-knappen i container-div:en
-
-innerModal.appendChild(saveButton); // Lägger till save-knappen i container-div:en
-container.appendChild(textSuggestionButton); // Lägger till textSuggestionButton i container-div:en
-
-
-/**
- * Skapar en ny text area och lägger till den i container-div:en
- */
-function openEmptyTextArea() {
-    let newTextArea = document.createElement("textarea");
-    container.appendChild(newTextArea);
-
-
-    innerModal.appendChild(newTextArea);
-}
-
-
-
-/**
- * Skapar en text area, lägger till ett textförslag och appendar den till container-div:en
- */
-function openSuggestionTextArea() {
-    let newPromptTextArea = document.createElement("textarea");
-    newPromptTextArea.value = randomTextSuggestion();
-    container.appendChild(newPromptTextArea);
-}
-function modal() {
-    let modalBg = document.createElement("div");
-    let innerModal = document.createElement("div");
-
-    modalBg.setAttribute("id", "")
-
-    innerModal.appendChild(newPromptTextArea);
-}
-
-let modalBg = document.createElement("div");
-
-function modal() {
-    
-    let body = document.querySelector("body");
-    modalBg.setAttribute("id", "modalBgBox");
-    
-    modalBg.style.backgroundColor = "rgba(0,0,0,0.4)"; /* Black w/ opacity */
-    modalBg.style.width = "100%";
-    modalBg.style.height = "100vh";
-    modalBg.style.position = "absolute";
-    modalBg.style.top = "0px";
-    modalBg.style.padding = "100px";
-
-    innerModal.style.backgroundColor = "white";
-    innerModal.style.width = "50%";
-    
-    
-    body.appendChild(modalBg);
-    modalBg.appendChild(innerModal);
-}
-
-function closeModal (e){
-    console.log("jag körs baby!");
-    if (e.target === modalBg){
-        modalBg.style.display = "none";
-    }
-}
-
-modalBg.addEventListener("click", closeModal);
-
-
-
-/**
- * Slumpar fram ett av 10 textförslag
- * Returnerar en string med ett textförslag
- */
-function randomTextSuggestion() {
-    let number = Math.round((Math.random() * 10) + 1);
-    let text;
-    switch (number) {
-        case 1:
-            text = "What did you hate most about 2020?";
-            break;
-        case 2:
-            text = "Where do you want to travel when Corona restrictions end?";
-            break;
-        case 3:
-            text = "What people have you missed during this year, and how do you plan to connect more with them in the future?";
-            break;
-        case 4:
-            text = "What have you learned about yourself during 2020?";
-            break;
-        case 5:
-            text = "Shows, music, artists you have discovered during 2020?";
-            break;
-        case 6:
-            text = "How has your life changed during 2020?";
-            break;
-        case 7:
-            text = "How have you taken care of yourself and others this year?";
-            break;
-        case 8:
-            text = "What has made you angry during 2020?";
-            break;
-        case 9:
-            text = "What do you dream about for 2021?";
-            break;
-        case 10:
-            text = "Who have you turned to this year? Why?";
-            break;
-        default:
-            text = "default";
-    }
-
-    return text;
-}
-
-/**
- * Döljer knapp
- */
-function hideButton(button) {
-    button.style.display = "none";
-}
-
-/**
- * Visar knapp
- */
-
-function showButton(button) {
-    button.style.display = "block";
-}
-
 saveButton.addEventListener("click", () => { //clear text area vid tryck på save
     clearTextArea();
 })
-
-emptyNoteButton.addEventListener("click", () => { // lägger till en eventlistener på New note-knappen
-    hideButton(emptyNoteButton);
-    hideButton(newEmptyListButton);
-    hideButton(textSuggestionButton);  // dessa tre funktioner körs vid klick: dölj new note-knappen, öppna textarea, visa save-knappen
-    showButton(saveButton);
-    showTextArea();
-});
-
-textSuggestionButton.addEventListener("click", () => {
-    hideButton(emptyNoteButton);
-    hideButton(newEmptyListButton);
-    hideButton(textSuggestionButton);
-    openSuggestionTextArea();
-    showButton(saveButton);
-});
     
 emptyNoteButton.addEventListener("click", () => { // lägger till en eventlistener på New note-knappen
-    //hideButton(emptyNoteButton);
-    //hideButton(newEmptyListButton);
-    //hideButton(textSuggestionButton);  // dessa tre funktioner körs vid klick: dölj new note-knappen, öppna textarea, visa save-knappen
     modal();
-    openEmptyTextArea();
+    showTextArea();
     showButton(saveButton);
 });
 
 textSuggestionButton.addEventListener("click", () => {
-    //hideButton(emptyNoteButton);
-    //hideButton(newEmptyListButton);
-    //hideButton(textSuggestionButton);
     modal();
     openSuggestionTextArea();
     showButton(saveButton); 
@@ -370,3 +306,17 @@ newEmptyListButton.addEventListener("click", () => {
     hideButton(textSuggestionButton);
     showButton(saveBtn);
 });
+
+modalBg.addEventListener("click", closeModal);
+
+/*
+//Add todays date to list note//
+let today = new Date();
+let date = today.getFullYear() + "-" + (today.getMonth()+1) + "-" + today.getDate();
+
+//constructor for note-object in list form//
+function ListNote(inputtitle, inputlistItem, date){
+    listTitle = inputtitle;
+    listItem = inputlistItem;
+    listDate = date;
+};*/
