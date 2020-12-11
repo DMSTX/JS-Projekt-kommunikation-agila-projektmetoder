e3718f45e3d334a161fccddf8edd697db3d78031
anderbecks <ludviganderbeck@gmail.com>
2020-12-11 09:35:19 +0100

    no emptyspace in titlebox

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ let pArray = []
 function Note(type) {
     this.date = date;
     this.type = type;
-    this.title = " ";
+    this.title = "";
     this.content = "nothing";
     this.addContent = function () {
         if (this.type === "list") {
